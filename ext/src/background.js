a1bd52f7d651629fc7d3bdc7b8335a6920bef3bd
estasney <estasney@users.noreply.github.com>
2018-01-02 13:28:18 -0500

    Update

--- ext/src/background.js
+++ ext/src/background.js
@@ -1,11 +1,12 @@
 // background.js
-var url = "http://127.0.0.1:5000/api/v1/profiles";
+// var url = "http://127.0.0.1:5000/api/v1/profiles";
+var url = "http://estasney1.pythonanywhere.com/api/v1/profiles";
 
 // pass content script data to external API
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "new_clip") {
         // if it's passing in new data...
-        console.log("New message received")
+        console.log("New message received");
         postData(url, request.id, request.purl, request.raw_html);
         // No sendResponse needed, send empty object
         sendResponse();
